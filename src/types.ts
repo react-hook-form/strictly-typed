7a5338c53110b14656da59e6e52669cedb6f38b7
Kotaro Sugawara <kotarella1110@gmail.com>
2020-06-21 23:39:17 +0900

    change generics paramter naming

--- src/types.ts
+++ src/types.ts
@@ -2,67 +2,68 @@ import { Control } from 'react-hook-form';
 
 export type ArrayWithLength<N extends number> = { [K in N]: any };
 
-export interface DeepPathArray<T, P> extends ReadonlyArray<string | number> {
-  ['0']?: keyof T;
-  ['1']?: P extends {
+export interface DeepPathArray<TValues extends Record<string, any>, TPath>
+  extends ReadonlyArray<string | number> {
+  ['0']?: keyof TValues;
+  ['1']?: TPath extends {
     ['0']: infer K0;
   }
-    ? K0 extends keyof T
-      ? keyof T[K0]
+    ? K0 extends keyof TValues
+      ? keyof TValues[K0]
       : never
     : never;
-  ['2']?: P extends {
+  ['2']?: TPath extends {
     ['0']: infer K0;
     ['1']: infer K1;
   }
-    ? K0 extends keyof T
-      ? K1 extends keyof T[K0]
-        ? keyof T[K0][K1]
+    ? K0 extends keyof TValues
+      ? K1 extends keyof TValues[K0]
+        ? keyof TValues[K0][K1]
         : never
       : never
     : never;
-  ['3']?: P extends {
+  ['3']?: TPath extends {
     ['0']: infer K0;
     ['1']: infer K1;
     ['2']: infer K2;
   }
-    ? K0 extends keyof T
-      ? K1 extends keyof T[K0]
-        ? K2 extends keyof T[K0][K1]
-          ? keyof T[K0][K1][K2]
+    ? K0 extends keyof TValues
+      ? K1 extends keyof TValues[K0]
+        ? K2 extends keyof TValues[K0][K1]
+          ? keyof TValues[K0][K1][K2]
           : never
         : never
       : never
     : never;
-  ['4']?: P extends {
+  ['4']?: TPath extends {
     ['0']: infer K0;
     ['1']: infer K1;
     ['2']: infer K2;
     ['3']: infer K3;
   }
-    ? K0 extends keyof T
-      ? K1 extends keyof T[K0]
-        ? K2 extends keyof T[K0][K1]
-          ? K3 extends keyof T[K0][K1][K2]
-            ? keyof T[K0][K1][K2][K3]
+    ? K0 extends keyof TValues
+      ? K1 extends keyof TValues[K0]
+        ? K2 extends keyof TValues[K0][K1]
+          ? K3 extends keyof TValues[K0][K1][K2]
+            ? keyof TValues[K0][K1][K2][K3]
             : never
           : never
         : never
       : never
     : never;
-  ['5']?: P extends {
+  ['5']?: TPath extends {
     ['0']: infer K0;
     ['1']: infer K1;
     ['2']: infer K2;
     ['3']: infer K3;
     ['4']: infer K4;
   }
-    ? K0 extends keyof T
-      ? K1 extends keyof T[K0]
-        ? K2 extends keyof T[K0][K1]
-          ? K3 extends keyof T[K0][K1][K2]
-            ? K4 extends keyof T[K0][K1][K2][K3]
-              ? keyof T[K0][K1][K2][K3][K4]
+    ? K0 extends keyof TValues
+      ? K1 extends keyof TValues[K0]
+        ? K2 extends keyof TValues[K0][K1]
+          ? K3 extends keyof TValues[K0][K1][K2]
+            ? K4 extends keyof TValues[K0][K1][K2][K3]
+              ? keyof TValues[K0][K1][K2][K3][K4]
               : never
             : never
           : never
@@ -72,33 +73,35 @@ export interface DeepPathArray<T, P> extends ReadonlyArray<string | number> {
 }
 
 export type DeepPathArrayValue<
-  T,
-  P extends DeepPathArray<T, P>
-> = P extends ArrayWithLength<0 | 1 | 2 | 3 | 4 | 5 | 6>
+  TValues extends Record<string, any>,
+  TPath extends DeepPathArray<TValues, TPath>
+> = TPath extends ArrayWithLength<0 | 1 | 2 | 3 | 4 | 5 | 6>
   ? any
-  : P extends ArrayWithLength<0 | 1 | 2 | 3 | 4 | 5>
-  ? T[P[0]][P[1]][P[2]][P[3]][P[4]][P[5]]
-  : P extends ArrayWithLength<0 | 1 | 2 | 3 | 4>
-  ? T[P[0]][P[1]][P[2]][P[3]][P[4]]
-  : P extends ArrayWithLength<0 | 1 | 2 | 3>
-  ? T[P[0]][P[1]][P[2]][P[3]]
-  : P extends ArrayWithLength<0 | 1 | 2>
-  ? T[P[0]][P[1]][P[2]]
-  : P extends ArrayWithLength<0 | 1>
-  ? T[P[0]][P[1]]
-  : P extends ArrayWithLength<0>
-  ? T[P[0]]
+  : TPath extends ArrayWithLength<0 | 1 | 2 | 3 | 4 | 5>
+  ? TValues[TPath[0]][TPath[1]][TPath[2]][TPath[3]][TPath[4]][TPath[5]]
+  : TPath extends ArrayWithLength<0 | 1 | 2 | 3 | 4>
+  ? TValues[TPath[0]][TPath[1]][TPath[2]][TPath[3]][TPath[4]]
+  : TPath extends ArrayWithLength<0 | 1 | 2 | 3>
+  ? TValues[TPath[0]][TPath[1]][TPath[2]][TPath[3]]
+  : TPath extends ArrayWithLength<0 | 1 | 2>
+  ? TValues[TPath[0]][TPath[1]][TPath[2]]
+  : TPath extends ArrayWithLength<0 | 1>
+  ? TValues[TPath[0]][TPath[1]]
+  : TPath extends ArrayWithLength<0>
+  ? TValues[TPath[0]]
   : never;
 
-export type DeepPath<T, P> = DeepPathArray<T, P> | keyof T;
+export type DeepPath<TValues extends Record<string, any>, TPath> =
+  | DeepPathArray<TValues, TPath>
+  | keyof TValues;
 
 export type DeepPathValue<
-  T,
-  P extends DeepPath<T, P>
-> = P extends DeepPathArray<T, P>
-  ? DeepPathArrayValue<T, P>
-  : P extends keyof T
-  ? T[P]
+  TValues extends Record<string, any>,
+  TPath extends DeepPath<TValues, TPath>
+> = TPath extends DeepPathArray<TValues, TPath>
+  ? DeepPathArrayValue<TValues, TPath>
+  : TPath extends keyof TValues
+  ? TValues[TPath]
   : any;
 
 export type FieldValuesFromControl<
@@ -140,7 +143,8 @@ export type ValidationRules<TFieldValue> = Partial<{
   validate: Validate<TFieldValue> | Record<string, Validate<TFieldValue>>;
 }>;
 
-export type Assign<T extends object, U extends object> = T & Omit<U, keyof T>;
+export type Assign<TValues extends object, U extends object> = TValues &
+  Omit<U, keyof TValues>;
 
 export type ControllerProps<
   TFieldValues extends Record<string, any>,
